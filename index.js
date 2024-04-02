879b844c617f5cab0032495e9c264b6beb2326a2
undefinedyobra <brianmelly008@gmail.com>
2024-04-02 11:20:40 +0300

    array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(array) {
+    const win = array.find(entry => entry.result === "W");
+    return win ? win.year : undefined;
+  }
+  
+  module.exports = superbowlWin;
